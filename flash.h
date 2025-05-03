0b8b7f01fe2108575fb82d1c027978c5c8577215
Alexander Mandera <alexander@mandera.eu>
2025-05-03 15:37:25 +0200

    Update flash.h

--- flash.h
+++ flash.h
@@ -1,3 +1,7 @@
+/**
+ * Parts of this code are from the ch32v30x_flash.c source by WCH, licensed under Apache 2.0
+ */
+
 #include "ch32fun.h"
 
 void FLASH_Unlock_Fast(void)
@@ -29,31 +33,27 @@ void FLASH_ProgramPage_Fast(uint32_t pageAddress, uint8_t *data)
 {
     uint8_t size = 64;
 
-    pageAddress &= 0xFFFFFF00;
+    Page_Address &= 0xFFFFFF00;
 
-    // Clear buffer and prep for flashing.
-	FLASH->CTLR = CR_PAGE_PG;  // synonym of FTPG.
-    FLASH->ADDR = (intptr_t)pageAddress;
-    printf( "FLASH->ADDR = %08lx\n", FLASH->ADDR );
-
-    while( FLASH->STATR & SR_BSY );  // No real need for this.
-    while( FLASH->STATR & SR_WR_BSY );  // No real need for this.
+    FLASH->CTLR |= CR_PAGE_PG;
+    while(FLASH->STATR & SR_BSY)
+        ;
+    while(FLASH->STATR & SR_WR_BSY)
+        ;
 
     while(size)
     {
-        *(uint32_t *)pageAddress = *(uint32_t *)data;
-        pageAddress += 4;
-        data += 1;
+        *(uint32_t *)Page_Address = *(uint32_t *)pbuf;
+        Page_Address += 4;
+        pbuf += 1;
         size -= 1;
         printf("To write %d bytes to %08lx\n", size, pageAddress);
-		while( FLASH->STATR & SR_WR_BSY );  // Only needed if running from RAM.
+        while(FLASH->STATR & SR_WR_BSY)
+            ;
     }
 
-    printf("Writing to flash\n");
-    // Actually write the flash out. (Takes about 3ms)
-	FLASH->CTLR = CR_PAGE_PG|CR_STRT_Set;
-
-    while(FLASH->STATR & FLASH_STATR_BSY);
-
+    FLASH->CTLR |= CR_PG_STRT;
+    while(FLASH->STATR & SR_BSY)
+        ;
     FLASH->CTLR &= ~CR_PAGE_PG;
 }
